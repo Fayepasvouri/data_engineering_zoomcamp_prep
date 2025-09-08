f9d256b732e18cdee0ecae272d6536569ada5e43
Faye Pasvouri <faye.pasvouri@omd.com>
2025-09-08 16:25:17 +0100

    Add Terraform configuration for Google Cloud resources and update Docker Compose port mapping

---
+++ week_one_basics_and_setup/1_terraform_gcp/terraform/terraform_basic/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "7.1.1"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("xxxxxxxxxxxx.json")
+  project     = "zinc-epigram-471514-s3"
+  region      = "europe-west2"
+}
+
+resource "google_storage_bucket" "data-lake" {
+  name          = "zinc-epigram-471514-s3-terra-bucket"
+  location      = "EU"
+  force_destroy = true
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 30 # days
+    }
+    action {
+      type = "Delete"
+    }
+  }
+}
+
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id = "ny_taxi"
+  project    = "zinc-epigram-471514-s3"
+  location   = "EU"
+}
