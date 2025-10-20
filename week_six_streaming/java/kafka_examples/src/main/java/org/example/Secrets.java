6d6c27a655deaeb94f3f38d3e111e89d24cb3f26
Faye Pasvouri <faye.pasvouri@omd.com>
2025-10-20 14:15:27 +0100

    secrets

---
+++ week_six_streaming/java/kafka_examples/src/main/java/org/example/Secrets.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Secrets {
+    public static final String KAFKA_CLUSTER_KEY = "Xxxxxxx";
+    public static final String KAFKA_CLUSTER_SECRET = "xxxxxxx";
+
+    public static final String SCHEMA_REGISTRY_KEY = "xxxxxx";
+    public static final String SCHEMA_REGISTRY_SECRET = "xxxxxxx";
+
+}
